226df295b0921f3503036ab1e8816d04b53d10df
Axel Brisse <axel.brisse@gmail.com>
2024-03-15 08:44:02 +0100

    default range is 1-1024, not 0-102, and do it faster

--- srcs/nmap.c
+++ srcs/nmap.c
@@ -9,10 +9,12 @@ void create_socket(nmap* nmap) {
     nmap->fd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP); // faudra un deuxieme socket pour les paquets UDP //  a reflechir dans l'avenir si on cree un socket pour chaque type de scan. en multi-threading avec 255 threads, on peut faire 255 scans en meme temps, mais donc faut-il 255 sockets en meme temps ? Ou poll va automatiquement gerer ca ? sur ping meme avec flood ça passe, donc bon
     if (nmap->fd < 0) error("Socket creation failed");
 
-    if (!(nmap->opt & OPT_PORTS))
-        for (int port = 0; port < 1024; port++) // for (int i = 0; i < 15; i++)  nmap->ports[i] = 1 << 64 - i; plus rapide
-            set_port(nmap->ports, port);
-    if (!(nmap->opt & OPT_SCAN)) nmap->opt |= 0b111111;
+    if (!(nmap->opt & OPT_PORTS)) {
+        for (int i = 0; i < 16; ++i) nmap->ports[i] = ~0;
+        nmap->ports[0] ^= 1;
+        nmap->ports[16] = 1;
+    }
+    if (!(nmap->opt & OPT_SCAN)) nmap->scan |= 0b111111;
 
     gettimeofday(&nmap->start_time, NULL);
     struct tm* tm = localtime(&nmap->start_time.tv_sec);
