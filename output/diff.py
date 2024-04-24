6008d24d5b4dbbc81cafdab2c133cccc65955804
Axel Brisse <axel.brisse@gmail.com>
2024-04-24 21:13:19 +0200

    diff

--- output/diff.py
+++ output/diff.py
@@ -1,3 +1,5 @@
+import sys
+
 RESET = "\x1b[0m"
 
 
@@ -15,8 +17,8 @@ def read_file(filename):
     return reports
 
 
-reports24 = read_file("24NEWNEW")
-reports250 = read_file("norandomise250")
+reports24 = read_file(sys.argv[1])
+reports250 = read_file(sys.argv[2])
 
 print("hosts in 24, not 250", set(reports24) - set(reports250))
 print("hosts in 250, not 24", set(reports250) - set(reports24))
@@ -35,10 +37,10 @@ for k in shared_keys:
         v250.pop(0)
     if v24 != v250:
         print(k)
-        print()
+        print("\n===", sys.argv[1], end=" ===\n\n")
         print("\n".join(v24))
-        print()
+        print("\n===", sys.argv[2], end=" ===\n\n")
         print("\n".join(v250))
-        print("\n=====================================\n")
+        print("\n=============================================================\n")
         count += 1
 print(count, "differences")
