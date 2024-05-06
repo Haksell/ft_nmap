7128e7ec13fcf09f0e00d8e7aedc3b588fc16c7a
Axel Brisse <axel.brisse@gmail.com>
2024-05-06 11:00:51 +0200

    --top_ports ok

--- scripts/top_ports.py
+++ scripts/top_ports.py
@@ -1,23 +1,35 @@
-from collections import defaultdict
 from heapq import nlargest
-from operator import itemgetter
 import re
 
-TOP_PORTS = 10  # TODO: 1024
+TOP_PORTS = 1024
+PROTOCOLS = ["mixed", "tcp", "udp"]
 
-tcp_probabilities = defaultdict(float)
-udp_probabilities = defaultdict(float)
-mixed_probabilities = defaultdict(float)
+probabilities = {p: dict() for p in PROTOCOLS}
 
 for line in open("scripts/etc_services").readlines():
     if fullmatch := re.fullmatch(r"[\w-]+\s+(\d+)/(tcp|udp)\s+(0.\d+).*", line.strip()):
         port, protocol, probability = fullmatch.groups()
         port = int(port)
         probability = float(probability)
-        mixed_probabilities[port] += probability
-        (tcp_probabilities if protocol == "tcp" else udp_probabilities)[port] += (
-            probability
-        )
+        probabilities[protocol][port] = probability
 
-for k, v in nlargest(TOP_PORTS, tcp_probabilities.items(), key=itemgetter(1)):
-    print(k, v)
+factor = sum(probabilities["udp"].values()) / sum(probabilities["tcp"].values())
+for port in set(probabilities["tcp"].keys()) | set(probabilities["udp"].keys()):
+    probabilities["mixed"][port] = probabilities["tcp"].get(
+        port, 0
+    ) * factor + probabilities["udp"].get(port, 0)
+
+
+def format_array(output, protocol):
+    output.append(f"static const uint16_t top_ports_{protocol}[MAX_PORTS] = {{")
+    pbp = probabilities[protocol]
+    for port in nlargest(TOP_PORTS, pbp, key=pbp.get):
+        output.append(f"    {port},")
+    output.append("};")
+    output.append("")
+
+
+output = ["#pragma once", "", '#include "ft_nmap.h"', ""]
+for protocol in PROTOCOLS:
+    format_array(output, protocol)
+open("include/top_ports.h", "w").write("\n".join(output))
