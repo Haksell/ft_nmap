c3be87f3148d04a6789e5800de223702c2c97117
Axel Brisse <axel.brisse@gmail.com>
2024-05-09 18:27:54 +0200

    simplify SCAN_WIN

--- srcs/capture_packets.c
+++ srcs/capture_packets.c
@@ -78,7 +78,7 @@ static void handle_tcp(t_thread_info* th_info, const u_char* packet, const struc
         case SCAN_FIN:
         case SCAN_XMAS: port_state = tcp->th_flags & TH_RST ? PORT_CLOSED : PORT_UNEXPECTED; break;
         case SCAN_WIN:
-            port_state = tcp->th_flags & TH_RST ? tcp->th_win ? PORT_OPEN : PORT_CLOSED : PORT_UNEXPECTED;
+            if (tcp->th_flags & TH_RST) port_state = tcp->th_win ? PORT_OPEN : PORT_CLOSED;
             break;
         case SCAN_CONN: break; // TODO: Axel: on ne veut pas que pcap lis le connect()
     }
