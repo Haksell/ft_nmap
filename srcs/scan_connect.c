a2904a3d374dc248aff26989bd5e4defda99e638
Axel Brisse <axel.brisse@gmail.com>
2024-05-10 00:34:25 +0200

    clean poll

--- srcs/scan_connect.c
+++ srcs/scan_connect.c
@@ -1,8 +1,5 @@
 #include "ft_nmap.h"
 
-#define MAX_CONCURRENT_CONNECT 512
-// TODO: fix with multithreading
-
 extern volatile sig_atomic_t run;
 
 static void set_port_and_host_state(t_thread_info* th_info, port_state port_state, uint16_t port) {
@@ -10,15 +7,12 @@ static void set_port_and_host_state(t_thread_info* th_info, port_state port_stat
     set_port_state(th_info, port_state, port);
 }
 
-// TODO: same code for UDP?
+void scan_connect(t_thread_info* th_info, uint16_t* loop_port_array) {
+    uint16_t port_count = th_info->nmap->port_count;
+    struct pollfd fds[port_count];
+    struct sockaddr_in targets[port_count];
 
-static void scan_connect_range(t_thread_info* th_info, uint16_t* loop_port_array, int start, int end) {
-    struct pollfd fds[end - start];
-    int nfds = 0;
-
-    struct sockaddr_in targets[end - start];
-
-    for (int port_index = start; port_index < end && run; ++port_index) {
+    for (int port_index = 0; port_index < port_count && run; ++port_index) {
         int port = loop_port_array[port_index];
         int fd = socket(AF_INET, SOCK_STREAM, 0);
         if (fd < 0) error("Connect socket creation failed");
@@ -30,61 +24,47 @@ static void scan_connect_range(t_thread_info* th_info, uint16_t* loop_port_array
         struct sockaddr_in target = {
             .sin_family = AF_INET,
             .sin_port = htons(port),
-            .sin_addr = th_info->hostaddr.sin_addr
-        };
-        targets[port_index - start] = target;
-        fds[nfds].fd = fd;
-        fds[nfds].events = POLLOUT;
-        nfds++;
+            .sin_addr = th_info->hostaddr.sin_addr};
+        targets[port_index] = target;
+        fds[port_index].fd = fd;
+        fds[port_index].events = POLLOUT;
 
-        if (connect(fd, (struct sockaddr*)&targets[port_index - start], sizeof(target)) == -1 && errno != EINPROGRESS) {
+        if (connect(fd, (struct sockaddr*)&targets[port_index], sizeof(target)) == -1 && errno != EINPROGRESS) {
             set_port_and_host_state(th_info, PORT_CLOSED, port);
         }
     }
 
-    int timeout = 2000 + 8 * (end - start); // Timeout in milliseconds
+    int timeout = 2000 + 8 * port_count; // TODO: better timeout
 
     while (run) {
-        int res = poll(fds, nfds, timeout);
+        int res = poll(fds, port_count, timeout);
         if (res == 0 || errno == EINTR) break;
         if (res < 0) error("poll failed");
 
-        for (int i = 0; i < nfds; ++i) {
+        for (int i = 0; i < port_count; ++i) {
+            // TODO: POLLOUT = PORT_OPEN, POLLERR = PORT_CLOSED
             if (fds[i].revents & POLLOUT || fds[i].revents & POLLERR) {
                 int so_error;
-                socklen_t len = sizeof(so_error);
-
-                getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR, &so_error, &len);
+                getsockopt(fds[i].fd, SOL_SOCKET, SO_ERROR, &so_error, &(socklen_t){sizeof(so_error)});
 
                 if (so_error == 0) {
-                    set_port_and_host_state(th_info, PORT_OPEN, loop_port_array[start + i]);
+                    set_port_and_host_state(th_info, PORT_OPEN, loop_port_array[i]);
                 } else if (so_error == ECONNREFUSED) {
-                    set_port_and_host_state(th_info, PORT_CLOSED, loop_port_array[start + i]);
+                    set_port_and_host_state(th_info, PORT_CLOSED, loop_port_array[i]);
                 }
 
                 close(fds[i].fd);
-                fds[i].fd = -1; // Mark as closed
+                fds[i].fd = -1;
             }
         }
 
         if (th_info->host->undefined_count[th_info->current_scan] == 0) break;
     }
 
-    for (int i = 0; i < nfds; ++i) {
+    for (int i = 0; i < port_count; ++i) {
         if (fds[i].fd != -1) {
             close(fds[i].fd);
         }
-    }
-}
-
-void scan_connect(t_thread_info* th_info, uint16_t* loop_port_array) {
-    for (uint16_t start = 0; start < th_info->nmap->port_count; start += MAX_CONCURRENT_CONNECT) {
-        scan_connect_range(
-            th_info,
-            loop_port_array,
-            start,
-            MIN(start + MAX_CONCURRENT_CONNECT, th_info->nmap->port_count)
-        );
     }
     set_default_port_states(th_info);
 }
